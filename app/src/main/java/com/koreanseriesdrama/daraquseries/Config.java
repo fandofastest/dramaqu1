95e51fe5882beae7e79806067068fa61b8512250
fando <fandofast@gmail.com>
2020-03-13 16:35:32 +0700

    Initial commit

---
+++ app/src/main/java/com/koreanseriesdrama/daraquseries/Config.java
@@ -0,0 +1,20 @@
+package com.koreanseriesdrama.daraquseries;
+
+
+
+public class Config {
+
+    // copy your api url from php admin dashboard & paste below
+    public static final String API_SERVER_URL = "http://dramaqu.bukatutup.site/api/";
+
+    //copy your api key from php admin dashboard & paste below
+    public static final String API_KEY = "lovb6y5k3vv4soq5e7qfpghr";
+
+    //copy your terms url from php admin dashboard & paste below
+    public static final String TERMS_URL = "http://dramaqu.bukatutup.site/terms/";
+
+    //default theme
+    public static boolean DEFAULT_DARK_THEME_ENABLE = false;
+
+
+}
